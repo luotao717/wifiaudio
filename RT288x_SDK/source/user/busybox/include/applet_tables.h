c06752bbb078013b35ea330b554645c4c467407e
root <hc1@sohu.com>
2013-12-29 23:56:19 +0800

    init 2

---
+++ RT288x_SDK/source/user/busybox/include/applet_tables.h
@@ -0,0 +1,138 @@
+/* This is a generated file, don't edit */
+
+#define NUM_APPLETS 42
+
+const char applet_names[] ALIGN1 = ""
+"[" "\0"
+"[[" "\0"
+"ash" "\0"
+"brctl" "\0"
+"cat" "\0"
+"chpasswd" "\0"
+"cp" "\0"
+"date" "\0"
+"echo" "\0"
+"expr" "\0"
+"free" "\0"
+"grep" "\0"
+"halt" "\0"
+"ifconfig" "\0"
+"init" "\0"
+"init" "\0"
+"kill" "\0"
+"killall" "\0"
+"login" "\0"
+"ls" "\0"
+"mkdir" "\0"
+"mount" "\0"
+"ping" "\0"
+"poweroff" "\0"
+"printf" "\0"
+"ps" "\0"
+"pwd" "\0"
+"reboot" "\0"
+"rm" "\0"
+"route" "\0"
+"sed" "\0"
+"sh" "\0"
+"sleep" "\0"
+"syslogd" "\0"
+"telnetd" "\0"
+"test" "\0"
+"top" "\0"
+"touch" "\0"
+"udhcpc" "\0"
+"udhcpd" "\0"
+"uptime" "\0"
+"vconfig" "\0"
+;
+
+int (*const applet_main[])(int argc, char **argv) = {
+test_main,
+test_main,
+ash_main,
+brctl_main,
+cat_main,
+chpasswd_main,
+cp_main,
+date_main,
+echo_main,
+expr_main,
+free_main,
+grep_main,
+halt_main,
+ifconfig_main,
+init_main,
+init_main,
+kill_main,
+kill_main,
+login_main,
+ls_main,
+mkdir_main,
+mount_main,
+ping_main,
+halt_main,
+printf_main,
+ps_main,
+pwd_main,
+halt_main,
+rm_main,
+route_main,
+sed_main,
+ash_main,
+sleep_main,
+syslogd_main,
+telnetd_main,
+test_main,
+top_main,
+touch_main,
+udhcpc_main,
+udhcpd_main,
+uptime_main,
+vconfig_main,
+};
+const uint16_t applet_nameofs[] ALIGN2 = {
+0x0000,
+0x0002,
+0x0005,
+0x0009,
+0x000f,
+0x0013,
+0x001c,
+0x001f,
+0x0024,
+0x0029,
+0x002e,
+0x0033,
+0x0038,
+0x003d,
+0x0046,
+0x004b,
+0x0050,
+0x0055,
+0x805d,
+0x0063,
+0x0066,
+0x006c,
+0x4072,
+0x0077,
+0x0080,
+0x0087,
+0x008a,
+0x008e,
+0x0095,
+0x0098,
+0x009e,
+0x00a2,
+0x00a5,
+0x00ab,
+0x00b3,
+0x00bb,
+0x00c0,
+0x00c4,
+0x00ca,
+0x00d1,
+0x00d8,
+0x00df,
+};
+#define MAX_APPLET_NAME_LEN 8
